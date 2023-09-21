87381c7d0097a57e9a9a42e126983df78d9bd86d
adrianhrb <adrian.herrera.br@gmail.com>
2023-09-21 11:24:38 +0100

    Changes on first excersise

--- DSW/ut0/max_product.py
+++ DSW/ut0/max_product.py
@@ -1,27 +1,26 @@
-def check_data(number: str, segment: int):
+def check_data(number: str, span: int):
     if not number.isnumeric():
-        raise ValueError("Caracteres no numericos")
-    if segment > len(number):
-        raise ValueError("Segmento mas grande que numero")
-    if 0 >= segment:
-        raise ValueError("Segmento negativo")
+        raise ValueError("Not numeric characters are not allowed")
+    if span > len(number):
+        raise ValueError("Span number should be short than sequence length")
+    if 0 >= span:
+        raise ValueError("Span value should be greater or equal to 1 ")
 
 
 def max_product(
-    number_to_check: str,
-    segment: int,
+    sequence: str,
+    span: int,
     final_prod: int = 0,
     data_checked: bool = False,
 ):
     if not data_checked:
-        check_data(number_to_check, segment)
-    if len(number_to_check) < segment:
+        check_data(sequence, span)
+    if len(sequence) < span:
         return final_prod
-    provisional_segment = number_to_check[:segment]
     provisional_prod = 1
-    for num in provisional_segment:
+    for num in sequence[:span]:
         provisional_prod *= int(num)
     if provisional_prod > final_prod:
         final_prod = provisional_prod
-    # Si no se ha levantado la excepcion los datos estan correctos, asi que pasamos checked directamente como True
-    return max_product(number_to_check[1:], segment, final_prod, True)
+    # If the exception has not been raised, the data is correct, so we checked value is now True.
+    return max_product(sequence[1:], span, final_prod, True)
