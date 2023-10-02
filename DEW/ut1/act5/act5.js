77a1f01aa8146cd0525d541508c36f0b41324395
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-02 20:22:27 +0100

    Excersise 5

---
+++ DEW/ut1/act5/act5.js
@@ -0,0 +1,14 @@
+function frenchMode() {
+    window.alert('Bienvenue sur ma page');
+    console.log('Vous avez choisi le français');
+}
+
+function englishMode() {
+    window.alert('Welcome to my page');
+    console.log('Your choose was english');
+}
+
+function spanishMode() {
+    window.alert('Bienvenido a mi página');
+    console.log('Elegiste español');
+}
