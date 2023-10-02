9819e7d3a0f2cc97e90f34325c6be4561771d6c6
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-02 20:14:26 +0100

    Excersise 4

---
+++ DEW/ut1/act4/act4.js
@@ -0,0 +1,16 @@
+function trueFunct(word, result) {
+    var text = document.getElementById(word)
+    var res = document.getElementById(result)
+    text.style.color = 'green'
+    res.style.color = 'green'
+    res.innerHTML = 'Correcto'
+}
+
+function falseFunct(word, result) {
+    var text = document.getElementById(word)
+    var res = document.getElementById(result)
+    text.style.color = 'red'
+    res.style.color = 'red'
+    res.innerHTML = 'Error'
+    
+}
