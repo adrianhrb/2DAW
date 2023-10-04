3cd5b96813e059cb343018c4daf76b4117a041fc
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-04 20:25:46 +0100

    Project advanced

--- DSW/mysite/blog/views.py
+++ DSW/mysite/blog/views.py
@@ -1,17 +1,24 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No Post found')
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, 'blog/post/detail.html', {'post': post})
