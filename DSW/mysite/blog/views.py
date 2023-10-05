78a99c23e710969cf4eef9727f175d08e9cb1730
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-05 10:14:30 +0100

    Changes on URL and email form

--- DSW/mysite/blog/views.py
+++ DSW/mysite/blog/views.py
@@ -1,15 +1,28 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+        else:
+            form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
--- DSW/mysite/mysite/settings.py
+++ DSW/mysite/mysite/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'adrian.herrera.br@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxx'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
