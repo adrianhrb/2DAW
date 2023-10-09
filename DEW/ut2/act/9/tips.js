67eec79ac4d717b783d5387f3f377cd91e064541
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-09 21:40:53 +0100

    More excersises uploaded

--- DEW/ut2/act/9/tips.js
+++ DEW/ut2/act/9/tips.js
@@ -2,8 +2,17 @@ function tipsCalculator() {
     var pay = document.getElementById('pay').value
     var percent = document.getElementById('percent').value
     var result = ''
-    if (percent == '30'){
-        result = pay * 0.70
+    if (percent == '10'){
+        result = pay * 0.10
+    }
+    else if (percent == '15'){
+        result = pay * 0.15
+    }
+    else if (percent == '20'){
+        result = pay * 0.20
+    }
+    else {
+        result = 0
     }
     document.getElementById('result').innerHTML = 'El cálculo de porpinas determina que tienes que dejar ' + Math.round(result) + ' €'
 }
