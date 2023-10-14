fda90e4b8cdb65a442b028401e79f6c8d4e885b5
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-14 14:20:46 +0100

    RSS Feed and sitempas working

--- DSW/mysite/mysite/settings.py
+++ DSW/mysite/mysite/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
