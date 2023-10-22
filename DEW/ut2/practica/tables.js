e6d9f126ee1f26b77183139b3cf1b14827dc1db8
adrihrb <adrihrb@xubuntu22.04.myguest.virtualbox.org>
2023-10-22 16:21:14 +0100

    Some corrections on DEW practice2

--- DEW/ut2/practica/tables.js
+++ DEW/ut2/practica/tables.js
@@ -1,24 +1,33 @@
 function tabla7() {
+  var toWriteResults = []
     for (i = 0; i <= 10; i++) {
       var res = 7 * i;
-      console.log("7 * " + i + " = " + res);
+      var toWrite = "7 * " + i + " = " + res
+      toWriteResults.push(toWrite)
     }
+    document.getElementById('7').innerHTML = toWriteResults.join('<br>');
   }
   
 function tablaSuma8() {
   var i = 0;
+  var toWriteResults = [];
   while (i <= 10) {
     var res = 8 + i;
-    console.log("8 + " + i + " = " + res);
+    var toWrite = "8 + " + i + " = " + res
+    toWriteResults.push(toWrite)
     i++;
   }
+  document.getElementById('8').innerHTML = toWriteResults.join('<br>');
 }
 
 function tablaDivide9() {
   var i = 0;
+  var toWriteResults = [];
   do {
     var res = 9 / i;
-    console.log("9 / " + i + " = " + res);
+    var toWrite = "9 / " + i + " = " + res
+    toWriteResults.push(toWrite)
     i++;
   } while (i <= 10);
+  document.getElementById('9').innerHTML = toWriteResults.join('<br>');
 }
