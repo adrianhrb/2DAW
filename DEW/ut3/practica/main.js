0620815bdd1ef2542c9bc63bffbe6e864fe6c8c7
adrianhrb <adrian.herrera.br@gmail.com>
2023-10-24 18:30:17 +0100

    Advancing ut3 practice

---
+++ DEW/ut3/practica/main.js
@@ -0,0 +1,51 @@
+// OTHER WINDOW CODE
+function newWindow() {
+    window.open('new_window.html', "new", "width=200,height=200,resizable=false");
+    document.getElementById('newinfo').innerHTML = 'Ejemplo de nueva ventana'
+    let url = window.location.href
+    let protocol = window.location.protocol
+    let navCode = navigator.appCodeName
+    if (navigator.javaEnabled == true) {
+        let javaAnswer = 'SI tiene java activado'
+    } else{
+        let javaAnswer = 'NO tiene java activado'
+    }
+    let textTOWrite = `Url: ${url}<br>Protocol: ${protocol}<br>Nombre en código del navegador: ${navCode}<br>${javaAnswer}`
+    document.getElementById('newinfo').innerHTML = textTOWrite
+    document.getElementById('myframe').src = 'https://bing.es'
+}
+
+
+
+// MAIN PAGE CODE
+let name = window.prompt('Cómo te llamas (nombre y apellidos)?: ').toLowerCase()
+let day = window.prompt('Qué día naciste?: ')
+let month = window.prompt('En qué 1 naciste?: ')
+let year = window.prompt('En que año naciste?: ')
+let nameChars = name.length
+let firstA = name.indexOf('a')
+let lastA = findLastA()
+function findLastA() {
+    for (let i = nameChars - 1; i >= 0; i--){
+        if (name[i].toLowerCase() == 'a') {
+            return i
+        }
+    }
+}
+let d = new Date()
+let myDate = new Date(year + ',' + month + ',' + day)
+let weekDays = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+let bornDay = weekDays[myDate.getDay()]
+let age = d.getFullYear() - parseInt(year)
+let shortName = name.slice(3)
+let upperName = name.toUpperCase()
+let coseno = Math.cos(180)
+let maxNum = Math.max(34,67,23,75,35,19)
+let randomNum = Math.floor(Math.random() * 100000)
+let texto = `Buenos dias ${name},<br>Tu nombre tiene ${nameChars} caracteres incluyendo espacios.<br>La primera A de tu nombre está en la posición ${firstA}.\
+<br>La últuma A de tu nombre está en la posición ${lastA}.<br>Tu nombre menos las 3 primeras letras es: ${shortName}.<br>Tu nombre todo en mayúsculas es ${upperName}.<br>\
+Tu edad es ${age}.<br>Naciste un feliz ${bornDay} del año ${year}<br>El coseno de 180 es: ${coseno}<br>El número mayor de (34,67,23,75,35,19) es: ${maxNum}<br>\
+Ejemplo de número al azar: ${randomNum}`
+window.alert(texto)
+window.onload = document.getElementById("info").innerHTML = texto
+window.onload = newWindow()
