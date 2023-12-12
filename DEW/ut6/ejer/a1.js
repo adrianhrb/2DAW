d072d8474a16c058b08f57dee8ef4cbd9a423b28
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-12 18:38:59 +0000

    Advancing excer

---
+++ DEW/ut6/ejer/a1.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let links = document.getElementsByTagName('a')
+    let linksCounter = links.length
+    let penultimate = links[linksCounter - 2]
+    let pruebaLink = 'http://prueba'
+    let pruebaLinkCounter = 0
+    for (let i=0; i<links.length; i++){
+        if (links[i].getAttribute('href')
+    }
+})
