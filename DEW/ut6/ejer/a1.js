de0996e2a222bf8008efd5f753e73c55737c011b
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-13 14:49:41 +0000

    Ex 1 with query selectors done

--- DEW/ut6/ejer/a1.js
+++ DEW/ut6/ejer/a1.js
@@ -5,6 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
     let pruebaLink = 'http://prueba'
     let pruebaLinkCounter = 0
     for (let i=0; i<links.length; i++){
-        if (links[i].getAttribute('href')
+        if (links[i].getAttribute('href') == pruebaLink){
+            pruebaLinkCounter++
+        }
     }
+    let linksThirdP = document.getElementsByTagName('p')[2].getElementsByTagName('a').length
+})
+
+// Ex. made with query*
+document.addEventListener('DOMContentLoaded', () => {
+    let links = document.getElementsByTagName('a')
+    let linksCounter = links.length
+    let penultimate = links[linksCounter - 2]
+    let pruebaLink = 'http://prueba'
+    let pruebaLinkCounter = 0
+    for (let i=0; i<links.length; i++){
+        if (links[i].getAttribute('href') == pruebaLink){
+            pruebaLinkCounter++
+        }
+    }
+    let linksThirdP = document.querySelectorAll('p')[2].querySelectorAll('a').length
 })
