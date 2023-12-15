08d0bcec15b88965202c9cfae6b1baf9a5568cb8
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-15 19:06:13 +0000

    Some comments added to js

--- DEW/ut6/pract/a1.js
+++ DEW/ut6/pract/a1.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Bucle para agregar clase seleccionado a los div que se 
+    // Bucle para agregar clase "seleccionado" a los div del color a elegir
     for (let i=1; i<=6; i++){
         let currentColor = `.color${i}`
         let currentElement = document.querySelector(currentColor)
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let lastElement = document.querySelector('.seleccionado')
             let lastElementColor = lastElement.getAttribute('class').split(' ')[0]
             let newClass = currentElement.getAttribute('class') + ' seleccionado'
-            lastElement.setAttribute('class', lastElementColor)
+            lastElement.setAttribute('class', lastElementColor) // Eliminamos "seleccionado" al último elemento
             currentElement.setAttribute('class', newClass)
         })
     }
@@ -21,13 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
         element.style.backgroundColor = `${colorSelected}`
     }
 
-    // Funcion para crear el tablero
-    function createBoard() {
-        for (let i=1; i<=1600; i++){
+    // Funcion para crear el tablero y agregar eventos a los píxeles
+    function createBoard(boardSize) {
+        let size = boardSize * boardSize
+        for (let i=1; i<=size; i++){
             let newElement = document.createElement('div')
             newElement.setAttribute('class', i)
             newElement.setAttribute('id', 'pixel')
-            newElement.style = 'width:10px;height:10px;border:1px solid black'
+            newElement.style = 'width:15px;height:15px;border:1px solid black'
             mainDiv.appendChild(newElement)
             // Evento para la primera pulsacion
             newElement.addEventListener('mousedown', () => {
@@ -49,23 +50,37 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Insertamos el "tablero" dinamicamente y agregamos los eventos a los "pixeles del tablero"
+    // Evento para el botón de crear 
     let mainDiv = document.querySelector('#zonadibujo')
-    mainDiv.style = 'display:grid;grid-template-columns: repeat(40, 0fr);'
-    document.querySelector('#create').addEventListener('click', () => {
+    document.querySelector('#create').addEventListener('click', () => { // Agregamos el evento al botón de crear tablero
+        let boardSize = parseInt(prompt('De cuanto quieres el tablero? Indicar solo un nº (Ej: 40)'))
+        mainDiv.style = `display:grid;grid-template-columns: repeat(${boardSize}, 0fr);` // Ordenamos por columnas según las que indique el usuario
         document.querySelector('input').style = 'display:none' // Ocultamos el boton una vez creado el tablero
+
+        // Creamos e insertamos al documento los bototnes para limpiar y resetear
         let clearButton = document.createElement('input')
+        let resetButton = document.createElement('input')
+        resetButton.value = 'Resetear'
+        resetButton.type = 'button'
+        resetButton.id = 'reset'
         clearButton.value = 'Limpiar tablero'
         clearButton.type = 'button'
         clearButton.id = 'clear'
-        document.querySelector('#button').appendChild(clearButton) // Agregamos el boton para limpiar el tablero
-        createBoard()
+        document.querySelector('#button').appendChild(clearButton) 
+        document.querySelector('#button').appendChild(resetButton)
+
+        // Agregamos eventos a los botones
         clearButton.addEventListener('click', () => {
             let allPixels = document.querySelectorAll('#pixel')
             allPixels.forEach(pixel => {
-                pixel.style = 'width: 10px; height: 10px; border: 1px solid black; background-color: white'
+                pixel.style = 'width: 15px; height: 15px; border: 1px solid black; background-color: white'
             })
             document.querySelector('#pincel').innerHTML = 'Haz click y manten pulsado para pintar varios pixeles'
         })
+        resetButton.addEventListener('click', () => {
+            location.reload()
+        })
+
+        createBoard(boardSize) // Finalmente llamamos a la función para crear el tablero
     })
 })
