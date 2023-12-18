71db93a0502cffddcc9d50923b9762467d540728
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-18 18:54:54 +0000

    Advancing next activity

---
+++ DOR/ut4/a2/code/3.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function init(){
+        var allImg = document.querySelectorAll('img')
+        for (let i=0; i < allImg.length; i++){
+            if (allImg[i].getAttribute('data-src')){
+                allImg[i].setAttribute('src', allImg[i].getAttribute('data-src'))
+            }
+        }
+    }
+    init()
+})
