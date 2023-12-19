bd80f4fa368e03ee7cc9695e8693b93861158bfa
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-19 16:42:12 +0000

    Advancing ut4 activity

--- DOR/ut4/a2/code/3.js
+++ DOR/ut4/a2/code/3.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     function init(){
-        var allImg = document.querySelectorAll('img')
+        var allImg = document.getElementsByTagName('img')
         for (let i=0; i < allImg.length; i++){
             if (allImg[i].getAttribute('data-src')){
                 allImg[i].setAttribute('src', allImg[i].getAttribute('data-src'))
