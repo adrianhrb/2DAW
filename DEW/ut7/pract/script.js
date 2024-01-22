86d011d9e7780444afb298b021ed2718835c9d74
adrianhrb <adrian.herrera.br@gmail.com>
2024-01-22 20:08:40 +0000

    some advances on practice

--- DEW/ut7/pract/script.js
+++ DEW/ut7/pract/script.js
@@ -1,22 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let rssUrl = 'https://www.eldia.es/rss/section/12502'
-    let url = 'server.php?rssUrl=' + rssUrl
+    let url = 'server.php?url='
+    let noticias = document.querySelector('#noticias')
+    let createButton = document.querySelector('#crearRSS')
+    let select = document.querySelector('#campoSelect')
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        for(var i=0;data.length;i++){
-            displayData(data.item[i])
-        }
+    // Evento para agregar nuevos RSS con nombre y url
+    createButton.addEventListener('click', () => {
+        let name = prompt('Como se va a llamar tu nuevo recurso?')
+        let newUrl = prompt('Cuál es el link del recurso?')
+        let option = new Option(name, newUrl)
+        select.appendChild(option)
+        sendRequest(url + newUrl)
     })
 
-    function displayData(object){
-        let title = object.title
-        let link = object.link
-        let description = object.description
-        let date = object.lastBuildDate
-        let divElement = document.querySelector('#datos')
-        divElement.innerHTML = `<p>${title}</p><p>${link}</p><p>${description}</p><p>${date}</p>`
-
+    // Petición al servidor con la url del recurso agregada
+    function sendRequest(url){
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            displayData(data)
+        })
     }
+
+    // Función para extraer los datos y representarlos en el div de noticias
+    function displayData(data){
+        noticias.innerHTML = ''
+        data.channel.item.forEach(element => {
+            let title = document.createElement('h1')
+            let description = document.createElement('p')
+            let date = document.createElement('p')
+            let link = document.createElement('a')
+            title.innerHTML = element.title
+            description.innerHTML = element.description
+            date.innerHTML = element.pubDate
+            link.innerHTML = element.link
+            link.href = element.link
+            noticias.appendChild(title)
+            noticias.appendChild(link)
+            noticias.appendChild(description)
+            noticias.appendChild(date)
+        });
+    }
+
 })
